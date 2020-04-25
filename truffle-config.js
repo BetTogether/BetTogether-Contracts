d2ca8344154ddf4835e85de919af9bc5f32d93f6
Brennan Fife <brenfife@MacBook-Pro.local>
2020-04-25 18:41:39 -0500

    updated dependencies and truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -1,17 +1,72 @@
+require("dotenv").config();
 const path = require("path");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const INFURA_ID = process.env.INFURA_KEY;
+const MNEMONIC = process.env.MNEMONIC;
 
 module.exports = {
-
   plugins: ["truffle-security"],
-
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     develop: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "*"
-    }
-  }
+      network_id: "*",
+    },
+    mainnet: {
+      provider: () => {
+        return new HDWalletProvider(
+          MNEMONIC,
+          `https://mainnet.infura.io/v3/${INFURA_ID}`
+        );
+      },
+      network_id: 1,
+      gas: 15000000,
+    },
+    ropsten: {
+      provider: () => {
+        return new HDWalletProvider(
+          MNEMONIC,
+          `https://ropsten.infura.io/v3/${INFURA_ID}`
+        );
+      },
+      network_id: 3,
+      gas: 15000000,
+    },
+    rinkeby: {
+      provider: () => {
+        return new HDWalletProvider(
+          MNEMONIC,
+          `https://rinkeby.infura.io/v3/${INFURA_ID}`
+        );
+      },
+      network_id: 4,
+      gas: 15000000,
+    },
+    goerli: {
+      provider: () => {
+        return new HDWalletProvider(
+          MNEMONIC,
+          `https://goerli.infura.io/v3/${INFURA_ID}`
+        );
+      },
+      network_id: 5,
+      gas: 15000000,
+    },
+    kovan: {
+      provider: () => {
+        return new HDWalletProvider(
+          MNEMONIC,
+          `https://kovan.infura.io/v3/${INFURA_ID}`
+        );
+      },
+      network_id: 42,
+      gas: 15000000,
+    },
+  },
+  compilers: {
+    solc: {
+      version: "0.6.6",
+    },
+  },
 };
