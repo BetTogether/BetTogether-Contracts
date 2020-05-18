9df58fd6aae15309fba2cf165a14a0f0b76a3f78
Andrew Stanger <andrew@stanger.org.uk>
2020-05-18 11:41:35 +0100

    change tabs to 4 spaces

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,23 +6,23 @@ import '@openzeppelin/contracts/access/Ownable.sol';
 
 
 contract Token is ERC20, Ownable {
-  using SafeMath for uint256;
+    using SafeMath for uint256;
 
-  constructor(string memory _tokenName) public ERC20(_tokenName, 'MB') {}
+    constructor(string memory _tokenName) public ERC20(_tokenName, 'MB') {}
 
-  function mint(address _account, uint256 _amount) external onlyOwner {
-    _mint(_account, _amount);
-  }
+    function mint(address _account, uint256 _amount) external onlyOwner {
+        _mint(_account, _amount);
+    }
 
-  function burn(address _account, uint256 _amount) external onlyOwner {
-    _burn(_account, _amount);
-  }
+    function burn(address _account, uint256 _amount) external onlyOwner {
+        _burn(_account, _amount);
+    }
 
-  ////////////////////////////////////
-  ///// BOILERPLATE FUNCTIONS ////////
-  ////////////////////////////////////
+    ////////////////////////////////////
+    ///// BOILERPLATE FUNCTIONS ////////
+    ////////////////////////////////////
 
-  receive() external payable {}
+    receive() external payable {}
 
-  fallback() external payable {}
+    fallback() external payable {}
 }
