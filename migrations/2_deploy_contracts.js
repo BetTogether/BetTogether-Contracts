791a9f6f356c5c29514d8b333cf33063216326fc
Brennan Fife <brenfife@gmail.com>
2020-06-24 08:40:14 -0500

    router address updated

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,16 +4,25 @@ const DaiMockup = artifacts.require('DaiMockup');
 const aTokenMockup = artifacts.require('aTokenMockup');
 const RealitioMockup = artifacts.require('RealitioMockup.sol');
 
-// kovan addresses
-const aaveCashAddressKovan = '0xFf795577d9AC8bD7D90Ee22b6C1703490b6512FD';
-const aaveAtokenAddressKovan = '0x58AD4cB396411B691A9AAb6F74545b2C5217FE6a';
-const aaveLendingPoolAddressKovan = '0x580D4Fdc4BF8f9b5ae2fb9225D584fED4AD5375c';
-const aaveLendingPoolCoreAddressKovan = '0x95D1189Ed88B380E319dF73fF00E479fcc4CFa45';
-const realitioAddressKovan = '0x50E35A1ED424aB9C0B8C7095b3d9eC2fb791A168';
-const uniswapRouterKovan = '0xf164fC0Ec4E93095b804a4795bBe1e041497b92a';
-const daiAddressKovan = '0xFf795577d9AC8bD7D90Ee22b6C1703490b6512FD';
+// KOVAN VARIABLES
+const AAVE_CASH_ADDRESS_KOVAN = '0xFf795577d9AC8bD7D90Ee22b6C1703490b6512FD';
+const AAVE_ATOKEN_ADDRESS_KOVAN = '0x58AD4cB396411B691A9AAb6F74545b2C5217FE6a';
+const AAVE_LENDING_POOL_ADDRESS_KOVAN = '0x580D4Fdc4BF8f9b5ae2fb9225D584fED4AD5375c';
+const AAVE_LENDING_POOL_CORE_ADDRESS_KOVAN = '0x95D1189Ed88B380E319dF73fF00E479fcc4CFa45';
+const REALITIO_ADDRESS_KOVAN = '0x50E35A1ED424aB9C0B8C7095b3d9eC2fb791A168';
+const UNISWAP_ROUTER_ADDRESS_KOVAN = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
+const DAI_ADDRESS_KOVAN = '0xFf795577d9AC8bD7D90Ee22b6C1703490b6512FD';
 
-// market details DUMMY/TESTING DATA ONLY, NOT FOR MAINNET
+// RINKEBY VARIABLES
+const AAVE_CASH_ADDRESS_RINKEBY = '';
+const AAVE_ATOKEN_ADDRESS_RINKEBY = '';
+const AAVE_LENDING_POOL_ADDRESS_RINKEBY = '';
+const AAVE_LENDING_POOL_CORE_ADDRESS_RINKEBY = '';
+const REALITIO_ADDRESS_RINKEBY = '0x3D00D77ee771405628a4bA4913175EcC095538da';
+const UNISWAP_ROUTER_ADDRESS_RINKEBY = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
+const DAI_ADDRESS_RINKEBY = '';
+
+// market details
 const marketOpeningTime = 0;
 const marketResolutionTime = 0;
 const arbitrator = '0xd47f72a2d1d0E91b0Ec5e5f5d02B2dc26d00A14D'; // kleros mainnet address.
@@ -22,7 +31,6 @@ const owner = '0xCb4BF048F1Aaf4E0C05b0c77546fE820F299d4Fe';
 const question = 'Who will win the 2020 US General Election␟"Donald Trump","Joe Biden"␟news-politics␟en_US';
 const eventName = 'Who will win the 2020 US General Election';
 
-// Currently deploying MBMarket directly. Ultimately it will deploy MBMarketFactory
 module.exports = function (deployer, network) {
   if (network === 'develop') {
     deployer.deploy(DaiMockup).then((deployedDai) => {
@@ -50,21 +58,21 @@ module.exports = function (deployer, network) {
     // factory deploy
     deployer.deploy(
       MBMarketFactory,
-      daiAddressKovan,
-      aaveAtokenAddressKovan,
-      aaveLendingPoolAddressKovan,
-      aaveLendingPoolCoreAddressKovan,
-      realitioAddressKovan,
-      uniswapRouterKovan
+      DAI_ADDRESS_KOVAN,
+      AAVE_ATOKEN_ADDRESS_KOVAN,
+      AAVE_LENDING_POOL_ADDRESS_KOVAN,
+      AAVE_LENDING_POOL_CORE_ADDRESS_KOVAN,
+      REALITIO_ADDRESS_KOVAN,
+      UNISWAP_ROUTER_ADDRESS_KOVAN
     );
     // market deploy
     // deployer.deploy(
     //   MBMarket,
-    //   daiAddressKovan,
-    //   aaveAtokenAddressKovan,
-    //   aaveLendingPoolAddressKovan,
-    //   aaveLendingPoolCoreAddressKovan,
-    //   realitioAddressKovan,
+    //   DAI_ADDRESS_KOVAN,
+    //   AAVE_ATOKEN_ADDRESS_KOVAN,
+    //   AAVE_LENDING_POOL_ADDRESS_KOVAN,
+    //   AAVE_LENDING_POOL_CORE_ADDRESS_KOVAN,
+    //   REALITIO_ADDRESS_KOVAN,
     //   eventName,
     //   marketOpeningTime,
     //   marketResolutionTime,
@@ -74,5 +82,15 @@ module.exports = function (deployer, network) {
     //   owner,
     //   true
     // );
+  } else if (network === 'rinkeby') {
+    deployer.deploy(
+      MBMarketFactory,
+      DAI_ADDRESS_RINKEBY,
+      AAVE_ATOKEN_ADDRESS_RINKEBY,
+      AAVE_LENDING_POOL_ADDRESS_RINKEBY,
+      AAVE_LENDING_POOL_CORE_ADDRESS_RINKEBY,
+      REALITIO_ADDRESS_RINKEBY,
+      UNISWAP_ROUTER_ADDRESS_RINKEBY
+    );
   }
 };
