7668b2913a9b987a80476b8f31d904388a617e31
Markus Waas <mail@markuswaas.de>
2020-08-09 20:35:51 +1300

    add sponsor functions

--- test/helpers.js
+++ test/helpers.js
@@ -2,6 +2,7 @@ const errors = {
   incorrectState: 'function cannot be called at this time',
   alreadyWithdrawn: 'Already withdrawn',
   oracleNotFinalised: 'Oracle has not finalised',
+  withdrawTooMuch: 'Cannot withdraw more than deposited',
 };
 
 module.exports = {errors};
